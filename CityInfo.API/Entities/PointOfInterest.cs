2ce4df4e5262e8a85dd096ff36680586f690ddf6
ixtlarle <SpaceCakes9@DESKTOP-QOKEHQE>
2018-08-15 19:12:01 -0500

    added a appsettings.productions with values to be used in the mailing services when the app is running in production mode
    began the creation of the entities as entity frameworkw ill be added to begin using actual data instead of in-memory data..added data annotation

---
+++ CityInfo.API/Entities/PointOfInterest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Entities
+{
+    public class PointOfInterest
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [ForeignKey("CityId")]//to my belief this was added to show that if we did not want to name
+        //the foreignkey in the convention base of xxxID we could add this and state wat it was 
+        //the the 2 below propertes to link the two entities as pointofinterest is dependent on city 
+        public City City { get; set; }
+
+        public int CityId { get; set; }
+
+    }
+}
