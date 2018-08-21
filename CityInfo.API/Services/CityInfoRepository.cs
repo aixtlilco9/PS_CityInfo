a101b523203fbc7a4c3eab635c06617d03e3f48f
ixtlarle <SpaceCakes9@DESKTOP-QOKEHQE>
2018-08-20 22:58:19 -0500

    introduced the repository pattern which will help in keeping duplicate code to a minimun as well as helping in testin code in the long run
    created an interface-->irepo which is the contract and the class-->repo which implemented the interface and then wrote the logic for the methods
    
    registered the repo and irepo

---
+++ CityInfo.API/Services/CityInfoRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CityInfo.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityInfo.API.Services
+{//it appears ienemerables need to execute the query with .tolist where as 
+    //the other methods use firstofdefault
+    public class CityInfoRepository : ICityInfoRepository
+    {
+        private CityInfoContext _ctx;
+        public CityInfoRepository(CityInfoContext ctx)
+        {
+            _ctx = ctx;
+        }
+        public IEnumerable<City> GetCities()
+        {
+            //calling tolist ensure the quesry is executed, calling tolist means iteration has to happen
+            return _ctx.Cities.OrderBy(c => c.Name).ToList();
+        }
+
+        public City GetCity(int cityId, bool includePointsOfInterest)
+        {
+            //calling .FirstOrDefault effectively executes the query
+            //to my understanding we use include to join the cities table with the points of interest 
+            //as in the initial in-memorydb when we did a get request for a city/cities we got both the city/cities along with the points of interest as they were together
+            //but now they are in seperate tables
+            if (includePointsOfInterest)
+            {
+                return _ctx.Cities.Include(c => c.PointsOfInterest).Where(c => c.Id == cityId).FirstOrDefault();
+            }
+            return _ctx.Cities.Where(c => c.Id == cityId).FirstOrDefault();
+
+        }
+
+        public PointOfInterest GetPointOfInterest(int cityId, int PointofInterestId)
+        {
+            return _ctx.PointOfInterest
+                .Where(p => p.Id == PointofInterestId && p.CityId == cityId).FirstOrDefault();
+        }
+
+        public IEnumerable<PointOfInterest> GetPointsOfInterest(int cityId)
+        {
+            return _ctx.PointOfInterest
+                            .Where(p => p.CityId == cityId).ToList();
+        }
+    }
+}
---
+++ CityInfo.API/Services/ICityInfoRepository.cs
@@ -0,0 +1,26 @@
+﻿using CityInfo.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Services
+{
+    public interface ICityInfoRepository
+    {
+        //returns all the cities
+        IEnumerable<City> GetCities();
+
+        //returns a single city
+        City GetCity(int cityId, bool includePointsOfInterest);
+
+        //returns the points of interest fora city
+        IEnumerable<PointOfInterest> GetPointsOfInterest(int cityId);
+
+        //returns a specific point of interest
+        PointOfInterest GetPointOfInterest(int cityId, int PointofInterestId);
+
+
+
+    }
+}
