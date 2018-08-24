1d930031a3a7b8422e3579d84f1cb449736151a9
ixtlarle <SpaceCakes9@DESKTOP-QOKEHQE>
2018-08-23 23:49:53 -0500

    started replacing the calls to the in memory db with calls to the repository..
    created a new dto as the old one had points of interest and a count and we choose to possibly not inclue the point of interest
    
    then injected the icity repo to the citiescontroller as that has access to the context and replaced the first get method which gets all cities and sorts them alphabetically
    
    the second method we gave the user the option to make a request which includes the point of interest or does not

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CityInfo.API.Models;
+using CityInfo.API.Services;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,31 +13,92 @@ namespace CityInfo.API.Controllers
     //there is no need to be specific in the method attributes
     public class CitiesController: Controller
     {
+        private ICityInfoRepository _cityInfoRepo;
+
+        public CitiesController(ICityInfoRepository cityInfoRepo)
+        {
+            _cityInfoRepo = cityInfoRepo;
+        }
         //[HttpGet("api/cities")]
         [HttpGet()]
         public IActionResult GetCities()
         {
-            return Ok(CitiesDataStore.Current.Cities);
-            //a not found is not necessary because even an empty collection is a valid response
-             
-            //the below is for now not needed since the cities data store was created which is used to hold our city data
+            //(this was initially used first )the below is for now not needed since the cities data store was created which is used to hold our city data
             //return new JsonResult(new List<object>()
             //{
             //    new {id=1, Name="New York City"},
             //    new {id=2, Name="Chicago" }
             //});
+
+            //the below line was implemented second
+            //return Ok(CitiesDataStore.Current.Cities);
+            //a not found is not necessary because even an empty collection is a valid response
+
+            var cityEntities = _cityInfoRepo.GetCities();
+            var results = new List<CityWithoutPointsOfInterestDto>();
+
+            //this method works by getting all the cities from the context then creating a newlist using the new dto and
+            //creating the new items in a specific way as determined by the c.w.p.o.i.dto and then returns them
+            foreach (var cityEntity in cityEntities)
+            {
+                results.Add(new CityWithoutPointsOfInterestDto
+                {
+                    Id = cityEntity.Id,
+                    Description = cityEntity.Description,
+                    Name = cityEntity.Name
+                });
+            }
+            return Ok(results);
         }
 
         [HttpGet("{id}")]
-        public IActionResult GetCity(int id)
+        public IActionResult GetCity(int id,bool includePointsOfInterest = false)
         {
-            var cityToReturn = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id);
-            if (cityToReturn == null)
+            var city = _cityInfoRepo.GetCity(id, includePointsOfInterest);
+            if(city == null)
             {
                 return NotFound();
             }
+            if (includePointsOfInterest)
+            {
+                var cityResult = new CityDto()
+                {
+                    Id = city.Id,
+                    Name = city.Name,
+                    Description = city.Description
+                };
 
-            return Ok(cityToReturn);
+                foreach (var poi in city.PointsOfInterest)
+                {
+                    cityResult.PointsOfInterest.Add(
+                        new PointOfInterestDto()
+                        {
+                            Id = poi.Id,
+                            Name = poi.Name,
+                            Description = poi.Description
+                        });
+                }
+                return Ok(cityResult);
+            }
+            else
+            {
+                var cityWithOutPointOfInterestResult =
+                    new CityWithoutPointsOfInterestDto()
+                    {
+                        Id = city.Id,
+                        Description = city.Description,
+                        Name = city.Name
+                    };
+                return Ok(cityWithOutPointOfInterestResult);
+            }
+            //---old cold that used inmemorydb-------------------------------
+            //var cityToReturn = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id);
+            //if (cityToReturn == null)
+            //{
+            //    return NotFound();
+            //}
+
+            //return Ok(cityToReturn);
         }
 
        
